169a7eb418f64fba0d529b3fd2ca7874d228a106
EunJaePark <design0728@naver.com>
2020-10-12 00:14:47 +0900

    update common.js
    - 변수 선언 부분이 발생주기로 인한 오류가 발생해 함수 안으로 변수를 넣어줌.

--- js/common.js
+++ js/common.js
@@ -8,12 +8,13 @@ function randomBtnClick() {
 
 
 // ------ menu 선택 함수. ------
-let btnBox = document.querySelector('.btnBox');
-let listBox = document.querySelector('.listBox');
-let menuPlus = document.querySelector('.menuPlus');
-let number = Math.floor(Math.random() * divideLsMenu().length); 
-let menuList = divideLsMenu(); 
 function menuClick() {
+    // let 변수들을 함수 바깥에서 선언해줬으나, divideLsMenu()함수로 인해 오류가 발생해 함수 안으로 넣어줌.
+    let btnBox = document.querySelector('.btnBox');
+    let listBox = document.querySelector('.listBox');
+    let menuPlus = document.querySelector('.menuPlus');
+    let number = Math.floor(Math.random() * divideLsMenu().length); 
+    let menuList = divideLsMenu(); 
     const audio = document.querySelector('audio');
     audio.play();   
 
@@ -139,6 +140,7 @@ function divideLsMenu() {
 
 // ------ local storage에 저장 된 메뉴목록 불러옴. ------
 function getLSmenu() {
+    console.log('getLSmneu', localStorage.getItem('menuList'));
     return localStorage.getItem('menuList');
 }
 
@@ -173,7 +175,7 @@ function init() {
         saveFirstLoadCount();
         // 처음 접속시에만 base menu 추가해줌.
         saveBaseMenu();
-    }
+    } 
     addLoadCount(); // 매번 접속시마다 loadCount를 1씩 늘려줌.
     inputSubmit();
     insertSavedMenu();
